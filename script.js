48e9eecdcf2ccc3efc10d9b16d5221ba426a34bd
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-28 20:43:50 -0800

    add properties and methods

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 class Hotel {
-  start() {}
+  constructor(name, rooms) {
+    this.name = name;
+    this.rooms = this.buildRoomsObject(rooms);
+  }
+
+  buildRoomsObject(rooms) {
+    let roomsObject = {};
+    rooms.forEach(room => {
+      const [name, capacity] = room;
+      roomsObject[name] = { capacity };
+    });
+    return roomsObject;
+  }
+
+  getName() {
+    return this.name
+      .split(' ')
+      .map(s => s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase())
+      .join(' ');
+  }
+
+  roomExists(roomName) {
+    return Object.keys(this.rooms).includes(roomName);
+  }
 }
 
-class Room {}
+class Room {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.occupants = [];
+  }
 
-const hotel = new Hotel();
-hotel.start();
+  isFull() {
+    return this.occupants.length === this.capacity;
+  }
+
+  availableSpace() {
+    return this.capacity - this.occupants.length;
+  }
+
+  addOccupant(occupant) {
+    if (this.isFull()) {
+      return false;
+    } else {
+      this.occupants.push(occupant);
+      return true;
+    }
+  }
+}
+
+const rooms = [
+  ['Basement', 4],
+  ['Attic', 2],
+  ['Under the Stairs', 1]
+];
+const hotel = new Hotel("hilbert's grand hotel", rooms);
+console.log(hotel);
+console.log(hotel.rooms['Basement'].capacity);
+console.log(hotel.getName());
+console.log(hotel);
+console.log(hotel.roomExists('Basement'));
+
+console.log(hotel.roomExists('Closet'));
